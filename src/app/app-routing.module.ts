509a0fc21a31d339e05b99be3b9d9c24c8c9ec5b
duytruong09 <nguyenduytruong760@gmail.com>
2023-06-18 22:54:01 +0700

    add router, module and component app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LopCreateComponent } from './lop/lop-create/lop-create.component';
+import { LopListComponent } from './lop/lop-list/lop-list.component';
+import { LopUpdateComponent } from './lop/lop-update/lop-update.component';
+import { SinhVienCreateComponent } from './sinh-vien/sinh-vien-create/sinh-vien-create.component';
 import { SinhVienListComponent } from './sinh-vien/sinh-vien-list/sinh-vien-list.component';
+import { SinhVienUpdateComponent } from './sinh-vien/sinh-vien-update/sinh-vien-update.component';
 
-const routes: Routes = [{
-  path: '',
-  component: SinhVienListComponent
-}];
-
+const routes: Routes = [
+  {
+    path: '',
+    component: SinhVienListComponent,
+  },
+  {
+    path: 'sinh-vien-create',
+    component: SinhVienCreateComponent,
+  },
+  {
+    path: 'sinh-vien-update/:id',
+    component: SinhVienUpdateComponent,
+  },
+  {
+    path: 'lops',
+    component: LopListComponent
+  },
+  {
+    path: 'lop-create',
+    component: LopCreateComponent
+  },
+  {
+    path: 'lops/lop-update/:id',
+    component: LopUpdateComponent,
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SinhVienListComponent } from './sinh-vien/sinh-vien-list/sinh-vien-list.component';
+import { SinhVienCreateComponent } from './sinh-vien/sinh-vien-create/sinh-vien-create.component';
+import { FormsModule } from '@angular/forms';
+import { SinhVienUpdateComponent } from './sinh-vien/sinh-vien-update/sinh-vien-update.component';
+import { LopListComponent } from './lop/lop-list/lop-list.component';
+import { LopCreateComponent } from './lop/lop-create/lop-create.component';
+import { LopUpdateComponent } from './lop/lop-update/lop-update.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SinhVienListComponent
+    SinhVienListComponent,
+    SinhVienCreateComponent,
+    SinhVienUpdateComponent,
+    LopListComponent,
+    LopCreateComponent,
+    LopUpdateComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
